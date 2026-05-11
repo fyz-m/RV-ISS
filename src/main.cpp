3a3982f8c30f31619745879e5a97f950ac4dc000
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-11 10:52:56 +0400

    Add CLI arg to print state

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,28 @@
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <string_view>
 
 
 constexpr int WIDTH = 32;
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 2) {
-    std::cerr << "\tUsage: ./rv_iss binaryfile.bin" << std::endl;
+  if (argc != 3) {
+    std::cerr << "\tUsage: ./rv_iss binaryfile.bin p=true/false" << std::endl;
     return 1;
   }
-  auto program = argv[1];
+  
+  bool print_state {false};
+  std::string_view p{argv[2]};
+  if (p == "p=true")
+    print_state = true;
+
 
   auto Instruction_Memory = std::make_shared <Memory>();
   auto Data_Memory = std::make_shared <Memory>();
 
+  auto program = argv[1];
   if (!Instruction_Memory->Load(program))
       return 1;
   
@@ -25,11 +32,13 @@ int main(int argc, char* argv[]) {
   cpu.runtoEbreak();
 
   std::string_view input_p { program};
-  if (input_p  ==  "tests/programs/factorial_12.bin")
-  {
+  if (input_p  ==  "tests/programs/factorial_12.bin") {
     std::cout << "Calculated 12! in " << cpu.cycles() << " cycles" << std::endl;
     std::cout << "Register a0 = " << cpu.readReg(10) << std::endl;
   }
 
+  if (print_state) 
+    cpu.printState();
+  
 } 
 
