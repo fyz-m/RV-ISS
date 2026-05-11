e57faa31e143ae0b5740a675824441a7e0e9f110
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-11 09:03:20 +0400

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,15 @@ int main(int argc, char* argv[]) {
   if (!Instruction_Memory->Load(program))
       return 1;
   
-
   CPU cpu (WIDTH, Instruction_Memory, Data_Memory);
-  
+  cpu.runtoEbreak();
+
+  std::string_view input_p { program};
+  if (input_p  ==  "tests/programs/factorial_12.bin")
+  {
+    std::cout << "Calculated 12! in " << cpu.cycles() << " cycles" << std::endl;
+    std::cout << "Register a0 = " << cpu.readReg(10) << std::endl;
+  }
+
 } 
 
